1232aa69540645ef89c8ab70ae98cb7270822c7a
Farid Adigamov <farid.adigamov@gmail.com>
2016-06-15 16:02:18 +0300

    skipped tests added

--- unittests/tests.py
+++ unittests/tests.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import warnings
+
 try:
     import unittest2 as unittest
 except ImportError:
@@ -14,18 +16,30 @@ class MyTestRunner(unittest.TextTestRunner):
 class MyTest(unittest.TestCase):
 
 	def test_one(self):
+		warnings.warn("i am fake warning")
 		self.assertTrue(True)
 
 	def test_two(self):
 		self.assertFalse(not True)
 
+	"""
 	def test_err(self):
 		self.assertEquals(1, 0)
+	"""
 
+	def test_raise(self):
+		raise unittest.SkipTest
+
+class AnotherTestCase(unittest.TestCase):
+
+	def test_another_one(self):
+		self.assertTrue(True)
 
 if __name__ == '__main__':
-#	suite = unittest.TestSuite()
-#	suite.addTest(unittest.makeSuite(MyTest))
+	suite = unittest.TestSuite()
+	suite.addTest(unittest.makeSuite(MyTest))
+	suite.addTest(unittest.makeSuite(AnotherTestCase))
 	runner = unittest.TextTestRunner(verbosity=2)
-	runner.run(unittest.makeSuite(MyTest))
+	runner.run(suite)
+#	runner.run(unittest.makeSuite(MyTest, AnotherTestCase))
 #	unittest.main()
