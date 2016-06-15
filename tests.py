267f9c178f364d780eff8bb80469fd7987ea0713
Farid Adigamov <farid.adigamov@gmail.com>
2016-06-15 13:06:13 +0300

    another test

--- tests.py
+++ tests.py
@@ -8,6 +8,9 @@ class MyTest(unittest.TestCase):
 	def test_one(self):
 		self.assertTrue(True)
 
+	def test_two(self):
+		self.assertFalse(not True)
+
 
 if __name__ == '__main__':
     unittest.main()
